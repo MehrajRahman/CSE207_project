44d35b5f9ef4a76f05d400b33a8071d80a27e52e
MehrajRahman <rahmanmehraj627@gmail.com>
2023-12-05 01:33:17 +0600

    last commit of tonight

--- headers/timer.h
+++ headers/timer.h
@@ -14,8 +14,6 @@
 
 void timer()
 {
-
-    // scanf("%d", &minutes);
     for (int i = 0; i >= 0; i--)
     {
         for (int j = 2; j >= 0; j--)
@@ -24,31 +22,5 @@ void timer()
         }
     }
 }
-
-
-// void stopwatch() {
-//     int seconds = 0;
-//     printf("Stopwatch started. Press 'q' to stop.\n");
-//     while (getchar() != 'q') {
-//         printf("Time elapsed: %d:%d\n", seconds / 60, seconds % 60);
-//         sleep(1); // sleep for 1 second
-//         seconds++;
-//     }
-//     printf("Stopwatch stopped.\n");
-// }
-
-// int main() {
-//     int choice;
-//     printf("1. Timer\n2. Stopwatch\nEnter your choice: ");
-//     scanf("%d", &choice);
-//     if (choice == 1) {
-//         timer();
-//     } else if (choice == 2) {
-//         stopwatch();
-//     } else {
-//         printf("Invalid choice.\n");
-//     }
-//     return 0;
-// }
 
 #endif
